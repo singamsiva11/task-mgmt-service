2f14a9f19d00bfa71adfbec5b99447ffb2e4dcbe
singamsiva11 <singamsivaprasadreddy123@gmail.com>
2024-06-02 10:29:14 +0530

    secondcommit

---
+++ src/main/java/com/ibank/taskmgmt/TaskMgmtApiApplication.java
@@ -0,0 +1,13 @@
+package com.ibank.taskmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMgmtApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMgmtApiApplication.class, args);
+	}
+
+}
